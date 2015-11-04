21a576315a256bd09c0c6ed0880f3d2c5bf6cf9a
Epicfern <kalir@live.fr>
2015-11-04 17:31:01 +0100

    ajout du travail de la semaine de moi et Koraxos

--- PartieClient/initialisation.php
+++ PartieClient/initialisation.php
@@ -4,82 +4,106 @@
 	$username ="root";
 	$password ="";
 	$db="myDB";
-	$sql = "CREATE DATABASE IF NOT EXISTS myDB";
+//	$sql = "CREATE DATABASE IF NOT EXISTS myDB";
 
     include'Donnees.inc.php';
 
 
 	$conn = mysqli_connect($servername, $username, $password);
-// Check connection
 
+mysqli_set_charset($conn,("UTF8"));
+// Check connection
 if (!$conn) {
     die(" Echec connection : " . mysqli_connect_error());
-
 }
 
 // creation base de données
 
-$sql = "CREATE DATABASE IF NOT EXISTS myDB";
-if(mysqli_query($conn,$sql))
-{
-	echo '<p class="erreur" >'."myDB cree/presente ".'</p>';
-} else {
-	echo '<p class="erreur" >'."erreur creation database" . mysqli_error($conn) .'</p>';
-}
+$sql = "CREATE DATABASE IF NOT EXISTS myDB ;";
+
+if(!mysqli_query($conn,$sql))
+{echo '<p class="erreur" >'."erreur creation database" . mysqli_error($conn) .'</p>';
+} 
 
 $conn = mysqli_connect($servername, $username, $password,$db);
 
 
+//changement de la base en utf 8
+$sql="ALTER DATABASE myDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;";
+if (!mysqli_query($conn, $sql)){
+ echo " erreur utf 8 " . mysqli_error($conn);
+}
+
 //===============
 //
-// Creation table recette + gestion erreur
+// Creation table recette + INDEX+ UTF8+ gestion erreur
 //
 //===============
 
-$sql ="CREATE TABLE RECETTE(
-idRecette VARCHAR(30) PRIMARY KEY,
+$sql ="CREATE TABLE IF NOT EXISTS RECETTE(
+idRecette int NOT NULL AUTO_INCREMENT PRIMARY KEY,     
+LibRecette VARCHAR(100) NOT NULL UNIQUE,
 ingredients VARCHAR(250) NOT NULL,
 preparation VARCHAR(1000) NOT NULL
 )
 ";
-if (mysqli_query($conn, $sql)) {
-    echo '<p class="erreur" >'." TABLE RECETTE cree ".'</p>' ;
-} else {
+if (!mysqli_query($conn, $sql)) {
     echo '<p class="erreur" >'."Erreur creation RECETTE: " . mysqli_error($conn).'</p>';
-}
+} 
+
+$sql="CREATE INDEX index_recette ON RECETTE (LibRecette)";
+
+if (!mysqli_query($conn, $sql)) {
+    echo '<p class="erreur" >'."Erreur INDEX RECETTE: " . mysqli_error($conn).'</p>';
+} 
+
+$sql="ALTER TABLE RECETTE CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;";
+if (!mysqli_query($conn, $sql)){
+ echo " erreur utf 8 " . mysqli_error($conn);
+} 
+//===============
+//
+// Creation table Aliment + INDEX + gestion utf8 +gestion erreur
+//
+//===============
+
+$sql="CREATE TABLE  IF NOT EXISTS ALIMENT(
+idAliment int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+LibALiment VARCHAR(40) NOT NULL UNIQUE 
+)";
+
+if (!mysqli_query($conn, $sql)) {
+     echo '<p class="erreur">'."Erreur creation ALIMENT: " . mysqli_error($conn).'</p>';
+} 
+
+$sql="CREATE INDEX index_aliment on ALIMENT (LibAliment)";
+
+if (!mysqli_query($conn, $sql)) {
+   echo '<p class="erreur" >'."Erreur INDEX ALIMENT: " . mysqli_error($conn).'</p>';
+} 
+
+// Changement de la table en utf 8
+$sql="ALTER TABLE ALIMENT CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;";
+
+if (!mysqli_query($conn, $sql)){
+ echo " erreur utf 8 " . mysqli_error($conn);
+} 
 
 //===============
 //
 // Creation table Contient + gestion erreur
-//
+// 
 //===============
 
-$sql="CREATE TABLE CONTIENT(
-idAliment VARCHAR(40) PRIMARY KEY,
-idRecette VARCHAR(30) REFERENCES RECETTE(idRecette)
+$sql="CREATE TABLE  IF NOT EXISTS CONTIENT(
+idAliment int REFERENCES ALIMENT(idAliment),
+idRecette int REFERENCES RECETTE(idRecette),
+CONSTRAINT recette_a_pour_aliment PRIMARY KEY(idAliment, idRecette)
 )
 ";
-if (mysqli_query($conn, $sql)) {
-    echo '<p class="erreur" >'."TABLE CONTIENT cree".'</p>';
-} else {
-    echo '<p class="erreur">'."Erreur creation CONTIENT: " . mysqli_error($conn).'</p>';
-}
-
-//===============
-//
-// Creation table Aliment + gestion erreur
-//
-//===============
-
-$sql="CREATE TABLE ALIMENT(
-idALiment VARCHAR(40) REFERENCES CONTIENT(idAliment)
-)";
-
-if (mysqli_query($conn, $sql)) {
-    echo '<p class="erreur" >'."TABLE ALIMENT cree".'</p>';
-} else {
-    echo '<p class="erreur">'."Erreur creation ALIMENT: " . mysqli_error($conn).'</p>';
-}
+if (!mysqli_query($conn, $sql)) {
+     echo '<p class="erreur">'."Erreur creation CONTIENT: " . mysqli_error($conn).'</p>';
+} 
 
 //===============
 //
@@ -87,18 +111,17 @@ if (mysqli_query($conn, $sql)) {
 //
 //===============
 
-$sql="CREATE TABLE ESTFILS(
-idAliment VARCHAR(40),
-SuperCat VARCHAR(40),
-CONSTRAINT alim_a_pour_pere PRIMARY KEY(idAliment, SuperCat)
+$sql="CREATE TABLE IF NOT EXISTS ESTFILS(
+idAliment int REFERENCES ALIMENT(idAliment),
+SousCat VARCHAR(40),
+CONSTRAINT alim_a_pour_pere PRIMARY KEY(idAliment, SousCat)
 )
 ";
 
-if (mysqli_query($conn, $sql)) {
-    echo '<p class="erreur" >'."TABLE ESTFILS cree".'</p>';
-} else {
-    echo '<p class="erreur">'."Erreur creation ESTFILS: " . mysqli_error($conn).'</p>';
+if (!mysqli_query($conn, $sql)) {
+   echo '<p class="erreur">'."Erreur creation ESTFILS: " . mysqli_error($conn).'</p>';
 }
+
 
 //===============
 //
@@ -106,51 +129,184 @@ if (mysqli_query($conn, $sql)) {
 //
 //===============
 
-$sql="CREATE TABLE ESTPERE(
-idAliment VARCHAR(40),
-SousCat VARCHAR(40),
-CONSTRAINT alim_a_pour_fils PRIMARY KEY(idAliment, SousCat)
+$sql="CREATE TABLE IF NOT EXISTS ESTPERE(
+idAliment int ,
+SuperCat VARCHAR(40),
+/*FOREIGN KEY(idAliment) REFERENCES ALIMENT(idAliment),    complété l'intégrité de la base*/
+CONSTRAINT alim_a_pour_fils PRIMARY KEY(idAliment, SuperCat)
 )
 ";
 
-if (mysqli_query($conn, $sql)) {
-    echo '<p class="erreur" >'."TABLE ESTFILS cree".'</p>';
-} else {
+if (!mysqli_query($conn, $sql)) {
     echo '<p class="erreur">'."Erreur creation ESTFILS: " . mysqli_error($conn).'</p>';
-}
+} 
+
+
+
+
 
 //on parcourt $hierarchie ( le tableau d'aliment)
 foreach($Hierarchie as $nom =>$description_categorie){
-    $nomalim=$nom;
-    echo $nomalim;
-    
 
-    // on recupère le tableau qui decrit les categories de l'aliment
-    $tab_description_categorie=array($description_categorie);
-    foreach($tab_description_categorie as $tab)
-       {
-        // on parcourt les deux sous tableaux de categories
-        foreach($tab as $x )
-           { 
-            foreach($x as $info)
-                {   //if(strcmp($x,'sous-categorie')==0)
-                   //{ 
-                    echo $info;}
-                   //}
+    $nomalim=mysqli_real_escape_string($conn,utf8_decode($nom));  
+    // Insertion des aliments dans ALIMENT
+    $sql="INSERT INTO ALIMENT (LibAliment) VALUES('$nomalim');";
 
-            }
-        echo '<p class="erreur">'.'</p>';
-       } 
-   /* foreach($nom as $tab_description_categorie=>$contenu)
-    {
-        echo $typecategorie;
-    }*/
+    if(!mysqli_query($conn, $sql))
+    {   echo'<p class="erreur" >'." Erreur Insertion ALiment " . mysqli_error($conn).'</p>'; }    
+
 }
 
 
+foreach($Hierarchie as $nom =>$description_categorie)
+{    
+    $nomalim=mysqli_real_escape_string($conn,utf8_decode($nom));
+    $tab_description_categorie=array($description_categorie);
+  
+    foreach($tab_description_categorie as $tab_categories)
+    {   // on parcourt les deux sous tableaux de categories grâce à $tab_categories
+        //====        
+        foreach($tab_categories as $type_categorie => $info_categorie)
+        { 
+        // typ_categorie contient sous-categorie ou super -categorie
+        //======
+        // info categorie est le tableau qui contient les aliments
+        // qui sont des sous cateogire/supercategories
+        //===
+        // on va voir dans $info_categorie qui est le tableau qui contient 
+        // les super ou les sous categorie selon le tableau où l'on se trouve
+       
+            $sql="SELECT idAliment , LibAliment
+            FROM  `aliment` 
+            WHERE LibAliment = '$nomalim'";
+            
+            $id_alim = mysqli_query($conn, $sql);     
+            // à faire seulement si $id_alima au moins un colonne mysqli_num_rows($id_alim) > 0
+            $res_id = mysqli_fetch_assoc($id_alim) ;  
+            $id_trouve=$res_id['idAliment'];
+
+            if(strcmp($type_categorie,'sous-categorie')==0)
+            {   
+                foreach($info_categorie as $nom_ss_categorie)
+                {
+                 $nom_ss_categorie=mysqli_real_escape_string($conn,utf8_decode($nom_ss_categorie));                      
+
+                 $sql="INSERT INTO ESTFILS (idAliment,SousCat) VALUES ( '$id_trouve' ,'$nom_ss_categorie')";
+
+                    if(!mysqli_query($conn,$sql))
+                    {    echo  '<p class="erreur">' . mysqli_error($conn).'</p>';                          
+                    }                                                       
+                }
+            }  
+            if(strcmp($type_categorie,'super-categorie')==0)  
+            {
+                foreach($info_categorie as $nom_sp_categorie)
+                {
+                    $nom_sp_categorie=mysqli_real_escape_string($conn,utf8_decode($nom_sp_categorie));
+
+                    $sql="INSERT INTO ESTPERE(idAliment,SuperCat) VALUES('$id_trouve','$nom_sp_categorie') ";
+
+                    if(!mysqli_query($conn,$sql))
+                    {    echo  '<p class="erreur">' . mysqli_error($conn).'</p>';                          
+                    }    
+                }    
+            } 
+        } 
+    }
+} 
+
+// fonction pour recette
+
+foreach($Recettes as $array_recette_courante)
+{
+	$recette=array($array_recette_courante);
+	
+		foreach($recette as $contenu)
+		{		$titre="";
+				$ingredients="";
+				$preparation="";
+					foreach($contenu as $libelle=>$info)
+					{
+					//$libelle représente le type d' information titre||ingredients||preparations||titre
+								if(strcmp($libelle,'titre')==0)
+								{ // recuperation pour variable $titre
+									$titre=mysqli_real_escape_string($conn,utf8_decode($info));								
+								}
+								if(strcmp($libelle,'ingredients')==0)
+								{// recuperation pour variable $ingredients	
+									$ingredients=mysqli_real_escape_string($conn,utf8_decode($info));
+								}	
+								if(strcmp($libelle,'preparation')==0)
+								{	// recuperation pour variable $preparation
+									$preparation=mysqli_real_escape_string($conn,utf8_decode($info));
+								}
+					}	
+		$sql="INSERT INTO RECETTE (LibRecette,ingredients,preparation) VALUES('$titre','$ingredients','$preparation')";	
+			if(!mysqli_query($conn,$sql));
+			{	 echo  '<p class="erreur">' . mysqli_error($conn).'</p>'; 				
+			}
+		}
+}  
+
+
+// fonction pour remplir contient
+
+foreach($Recettes as $array_recette_courante) 
+{
+	$recette=array($array_recette_courante);
+	
+	foreach($recette as $contenu)
+	{	
+		
+		foreach($contenu as $libelle => $array_aliment_recette)
+		{//$libelle représente le type d' information titre||ingredients||preparations||titre
+		
+			if((strcmp($libelle,'titre')==0))
+			{ //recuperation pour variable $titre		  
+			$titre=mysqli_real_escape_string($conn,utf8_decode($array_aliment_recette));		
+			$LibRecette=$titre;	
+			echo $LibRecette;
+			$sql="SELECT idRecette , LibRecette
+            FROM  `RECETTE` 
+            WHERE LibRecette = '$LibRecette'";
+			
+			$id_recette=mysqli_query($conn,$sql);
+			$res_id_recette = mysqli_fetch_assoc($id_recette) ;  
+            $id_recette_trouve=$res_id_recette['idRecette'];
+			
+			echo $id_recette_trouve. " " ;
+			
+			}
+		
+			if(strcmp($libelle,'index')==0)
+			{		
+				foreach($array_aliment_recette as $aliment_recette)
+				{
+					$LibAliment=mysqli_real_escape_string($conn,utf8_decode($aliment_recette));
+					
+					
+					$sql="SELECT idAliment , LibAliment
+					FROM ALIMENT
+					WHERE LibALiment='$LibAliment'";
+					
+					
+					$id_aliment= mysqli_query($conn,$sql);
+					$res_id_aliment= mysqli_fetch_assoc($id_aliment);
+					$id_aliment_trouve=$res_id_aliment['idAliment'];
+					
+					$sql="INSERT INTO CONTIENT(idALiment,idRecette) VALUES('$id_aliment_trouve','$id_recette_trouve')";
+					
+					if(!mysqli_query($conn,$sql))
+					{	 echo  '<p class="erreur">' . mysqli_error($conn).'</p>'; 				
+					}
+				}
+			}			
+		}
+	}	
+}
 /*
 $sql="DROP DATABASE myDB";
-mysqli_query($conn,$sql);*/
+ mysqli_query($conn,$sql);*/
 
 mysqli_close($conn);
 	
--- php/README.md
+++
@@ -1,2 +0,0 @@
-# cubalibre
-cubalibre
